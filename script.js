81b0a767661c0e74b21de80ca911521e296e3da7
Andrei Scafi <andreiscafi@hotmail.com>
2024-01-23 16:45:01 -0300

    Adding javascript code part 5.

--- script.js
+++ script.js
@@ -45,15 +45,16 @@ function addEntry() {
 
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
 
-    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
 
     const HTMLString = `
-    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
-    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories">
-    `;
+        <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+        <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+        <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+        <input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories"/>`;
 
-    targetInputContainer.innerHTML += HTMLString;
+    targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
+
+addEntryButton.addEventListener('click', addEntry);
 // End of Add Entry
