fdd07ef395daadb126359455b463a3ddd20ec3ae
Andrei Scafi <andreiscafi@hotmail.com>
2024-01-24 08:03:17 -0300

    Adding Javascript code part 7.

--- script.js
+++ script.js
@@ -77,3 +77,24 @@ function getCaloriesFromInputs(list) {
     return calories;
 }
 // End of getting calories from inputs
+
+// Calculating Calories
+
+function calculateCalories(e) {
+    e.preventDefault();
+    isError = false;
+    const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type=number]");
+    const lunchNumberInputs = document.querySelectorAll("#lunch input[type=number]");
+    const dinnerNumberInputs = document.querySelectorAll("#dinner input[type=number]");
+    const snacksNumberInputs = document.querySelectorAll("#snacks input[type=number]");
+    const exerciseNumberInputs = document.querySelectorAll("#exercise input[type=number]");
+
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+    const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+    const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+    const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+    const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+}
+// End of calculating Calories
